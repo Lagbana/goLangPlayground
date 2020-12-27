e6e5e13d2f861ed7d651021614ea80f5f1d09b04
Larry Agbana <larryagbana@gmail.com>
2020-12-27 12:22:48 -0500

    hello world and test

---
+++ hello_world/hello_test.go
@@ -0,0 +1,11 @@
+package hello
+
+import "testing"
+
+// TestHello for testing hello script
+func TestHello(t *testing.T) {
+	want := "Hello, world."
+	if got := Hello(); got != want {
+		t.Errorf("Hello() = %q, want %q", got, want)
+	}
+}
